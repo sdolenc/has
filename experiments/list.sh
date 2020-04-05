6def384cdf163934f2798e4063f2cd1ef755475d
localstepdo <sdolenc@fakeemail.com>
2020-04-04 17:04:47 -0700

    script listing all supported packages

---
+++ experiments/list.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+pushd "$(dirname "${BASH_SOURCE[0]}")"
+
+grep -o "^ \+[a-zA-Z0-9_|-]\+)" ../has | grep -o "[a-zA-Z0-9_|-]\+" | tr "|" "\n" | sort > list.txt
+
+popd
