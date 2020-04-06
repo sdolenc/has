895270d95bd4a06076f6c2d3bf763bba83ef8eb0
localstepdo <sdolenc@fakeemail.com>
2020-04-05 22:26:23 -0700

    missing

--- experiments/get_bats.sh
+++ experiments/get_bats.sh
@@ -15,7 +15,7 @@ if command -v apt-get 2>&1 >/dev/null; then
     apt-get update -qq && apt-get install -y -qq curl
 else
     apk add curl
-    # already has awk bash bzip2 grep gzip
+    # already has awk bash bzip2 grep gzip sed tar unzip wget xz
     # apk add --no-cache ack curl bzr docker emacs file gcc git go gradle hugo jq make nano npm perl php pv python python3 R ruby sudo tree vim yarn zip zsh
 fi
 
