9347e5dd2dd65f5fb5862d60590622b03cad7b16
localstepdo <sdolenc@fakeemail.com>
2020-04-07 23:01:58 -0700

    cleanup and temporarily disable tests

--- tests/get_bats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-
-commit="87b16eb"; \
-curl -L "https://github.com/bats-core/bats-core/tarball/${commit}" | tar xz; \
-"bats-core-bats-core-${commit}/install.sh" /usr/local && \
-command -v bats
