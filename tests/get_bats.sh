f369301af24b53781b05765df1e6fb355ef6b358
localstepdo <sdolenc@fakeemail.com>
2020-04-07 20:12:07 -0700

    simplify bats install

--- tests/get_bats.sh
+++ tests/get_bats.sh
@@ -4,28 +4,11 @@ set -euo pipefail
 # todo: move all of this to dockerfile
 
 # settings
-test_framework="https://github.com/bats-core/bats-core"
 commit="87b16eb"
-destination_root="/tmp"
-destination_path="bats-core"
-download_destination="${destination_root}/${destination_path}"
 
 # download
-if [ ! -d ${download_destination} ]; then
-    PREVPWD=$(pwd)
-    cd "${destination_root}"
-
-    curl -L "${test_framework}/tarball/${commit}" | tar xz
-    mv "bats-core-bats-core-${commit}" "${destination_path}"
-
-    cd "${PREVPWD}"
-fi
+curl -L "https://github.com/bats-core/bats-core/tarball/${commit}" | tar xz
 
 # install
-if command -v bash >/dev/null 2>&1; then
-    if ! "${download_destination}/install.sh" /usr/local; then
-        echo "retrying with root privileges"
-        sudo "${download_destination}/install.sh" /usr/local
-    fi
-    command -v bats
-fi
+"bats-core-bats-core-${commit}/install.sh" /usr/local
+command -v bats
