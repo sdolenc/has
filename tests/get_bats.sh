e5eee7a941a20296f936c3b402667d03e27c0e37
localstepdo <sdolenc@fakeemail.com>
2020-04-07 22:50:18 -0700

    prepare bats install for dockerfile

--- tests/get_bats.sh
+++ tests/get_bats.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 set -euo pipefail
 
-# todo: move all of this to dockerfile
-
-# settings
-commit="87b16eb"
-
-# download
-curl -L "https://github.com/bats-core/bats-core/tarball/${commit}" | tar xz
-
-# install
-"bats-core-bats-core-${commit}/install.sh" /usr/local
+commit="87b16eb"; \
+curl -L "https://github.com/bats-core/bats-core/tarball/${commit}" | tar xz; \
+"bats-core-bats-core-${commit}/install.sh" /usr/local && \
 command -v bats
