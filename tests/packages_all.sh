a30984770f71448731d69e9688d5990c22314644
localstepdo <sdolenc@fakeemail.com>
2020-04-12 14:20:59 -0700

    fix sort order

--- tests/packages_all.sh
+++ tests/packages_all.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 pushd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null
 
-grep -o "^ \\+[a-zA-Z0-9_|-]\\+)" ../has | grep -o "[a-zA-Z0-9_|-]\\+" | tr "|" "\\n" | sort
+grep -o "^ \\+[a-zA-Z0-9_|-]\\+)" ../has | grep -o "[a-zA-Z0-9_|-]\\+" | tr "|" "\\n" | sort --ignore-case
 
 popd >/dev/null
